64fd1ec0cdc93f8efc598f46704e1cc28b68cbe5
Ilia Loginov <loginoviv43@gmail.com>
2023-07-17 14:09:44 +0200

    add bootstrap styles

--- hexlet-chat/src/components/LoginForm.jsx
+++ hexlet-chat/src/components/LoginForm.jsx
@@ -1,45 +1,56 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import AuthContext from "./AuthContext";
-// import Main from "../routes/Main";
+import Button from "react-bootstrap/Button";
 
 function LoginForm() {
+    const [error, setError] = useState('');
     const navigate = useNavigate();
     const { isAuthorized, setAuthorized } = useContext(AuthContext);
 
     return (
-        <div>
-            <h1>Login form</h1>
-            <Formik
-                initialValues={{
-                    username: "",
-                    password: ""
-                }}
-                validationSchema={loginSchema}
-                onSubmit={async ({ username, password }) => {
-                    // console.log({ username, password })
-                    await axios
-                        .post('/api/v1/login', { username, password })
-                        .then((response) => localStorage.setItem("userToken", response.data.token))
-                        .then(() => setAuthorized(true))
-                        .catch((e) => console.log(e.message));
-                    if (isAuthorized) {
-                        navigate("/");
-                    };
+        <div className="container ">
+            <div className="text-center">
+                <h1 className="">Авторизация</h1>
+            </div>
+                <Formik
+                    initialValues={{
+                        username: "",
+                        password: ""
                     }}
-            >
-                <Form>
-                    <label>Login</label>
-                    <Field type="text" name="username" />
-                    <label>Password</label>
-                    <Field type="password" name="password" />
-                    <button type="submit">Submit</button>
-                </Form>
-            </Formik>
-        </div>
+                    validationSchema={loginSchema}
+                    onSubmit={async ({ username, password }) => {
+                        await axios
+                            .post('/api/v1/login', { username, password })
+                            .then((response) => localStorage.setItem("userToken", response.data.token))
+                            .then(() => setAuthorized())
+                            .then(() => alert("successfull authorization!"))
+                            .then(() => navigate("/"))
+                            .catch((e) => setError(e.message));
+
+                        }}
+                >
+                    <div className="row justify-content-center">
+                        <Form className="col-lg-6 border py-2">
+                            <div className="py-2">
+                                <label className="form-label">Введите имя:</label>
+                                <Field className="form-control" type="text" name="username" />
+                            </div>
+                            <div className="py-2">
+                                <label className="form-label">Введите пароль:</label>
+                                <Field className="form-control" type="password" name="password" />
+                            </div>
+                            {error ? <p className="text-danger my-0">{error}</p> : null}
+                            <div className="text-center py-2">
+                                <Button className="px-4" variant="primary" type="submit">Войти</Button>
+                            </div>
+                        </Form>
+                    </div>
+                </Formik>
+            </div>
     )
 }
 
