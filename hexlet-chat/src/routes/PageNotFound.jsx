1df091f940b847d98362afb9b757204d5fc954b3
Ilia Loginov <loginoviv43@gmail.com>
2023-07-20 15:28:40 +0200

    style 404page

--- hexlet-chat/src/routes/PageNotFound.jsx
+++ hexlet-chat/src/routes/PageNotFound.jsx
@@ -1,5 +1,22 @@
+import Button from "react-bootstrap/esm/Button";
+import { useNavigate } from "react-router-dom";
+
 function PageNotFound() {
-    return <div>404 PAGE NOT FOUND</div>
+    const navigate = useNavigate();
+    const HandleClick = () => navigate("/");
+
+    return (
+        <div className="row vh-100 align-items-center">
+            <div className="text-center aligh-middle">
+                <h1>
+                    404
+                    <br />
+                    Страница не найдена
+                </h1>
+                <Button onClick={HandleClick}>Вернуться на главную</Button>
+            </div>
+        </div>
+    );
 }
 
 export default PageNotFound;
